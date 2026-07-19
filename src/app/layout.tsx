8e7dd74202b4c9ecdfffeff837d9212dddf62642
ushna2922-lang <ushna2922@gmail.com>
2026-07-20 03:28:00 +0500

    Add Microsoft Clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ import PWARegister from "@/components/pwa-register";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-
+import Script from "next/script";
 
 
 const inter = Inter({
@@ -142,6 +142,15 @@ return (
       </ThemeProvider>
       <Analytics />
       <SpeedInsights />
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+  {`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "YOUR_PROJECT_ID");
+  `}
+</Script>
     </body>
     <GoogleAnalytics gaId="G-JJ7QKKN0FN" />
   </html>
